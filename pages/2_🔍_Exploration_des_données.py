9b359aae99009df0c01c3d58771db9227ae39e3b
acarot <carot.alex@gmail.com>
2024-07-24 22:05:21 +0200

    2_🔍_Exploration_des_données.py

--- pages/2_🔍_Exploration_des_données.py
+++ pages/2_🔍_Exploration_des_données.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
